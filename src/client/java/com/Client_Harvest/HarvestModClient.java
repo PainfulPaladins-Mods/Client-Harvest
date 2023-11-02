ef43628b8064ab8b98ba089d7b35102f9dfbced5
PainfulPaladins <122171814+painfulpaladins@users.noreply.github.com>
2023-11-02 15:28:17 +0200

    added: the main feature

---
+++ src/client/java/com/Client_Harvest/HarvestModClient.java
@@ -0,0 +1,53 @@
+package com.Client_Harvest;
+
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.CocoaBlock;
+import net.minecraft.block.CropBlock;
+import net.minecraft.block.NetherWartBlock;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.world.World;
+
+
+public class HarvestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        ClientTickEvents.END_CLIENT_TICK.register(this::clientTickEvent);
+        UseBlockCallback.EVENT.register(this::onBlockUse);
+    }
+
+    public static MinecraftClient client;
+    private void clientTickEvent(MinecraftClient mc) {
+        client = mc;
+    }
+
+    private static boolean isMature(BlockState state) {
+        if (state.getBlock() instanceof CocoaBlock) {
+            return state.get(CocoaBlock.AGE) >= CocoaBlock.MAX_AGE;
+        } else if (state.getBlock() instanceof CropBlock cropBlock) {
+            return cropBlock.isMature(state);
+        } else if (state.getBlock() instanceof NetherWartBlock) {
+            return state.get(NetherWartBlock.AGE) >= 3;
+        }
+
+        return false;
+    }
+    public ActionResult onBlockUse(PlayerEntity player, World world, Hand hand, BlockHitResult hitResult) {
+        return onBlockUse(player, world, hand, hitResult, true);
+    }
+    private ActionResult onBlockUse(PlayerEntity player, World world, Hand hand, BlockHitResult hitResult, boolean initialCall) {
+        BlockState state = world.getBlockState(hitResult.getBlockPos());
+        if (!isMature(state)) return ActionResult.PASS;
+        ClientPlayerInteractionManager interaction = new ClientPlayerInteractionManager(client, client.getNetworkHandler());
+        interaction.attackBlock(hitResult.getBlockPos(),hitResult.getSide());
+        return ActionResult.SUCCESS;
+    }
+}
